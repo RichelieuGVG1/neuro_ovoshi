d9a33112ce83df87b45d2cbf687f6a87ba33e2bc
Vasily Guryanov <62996833+RichelieuGVG1@users.noreply.github.com>
2024-03-30 22:46:39 +0300

    Add files via upload

---
+++ taxonomy/taxonomy_initializer.py
@@ -0,0 +1,8 @@
+from taxonomy_preprocess import main as mainn
+
+def main():
+	input_name = input('Enter certain name of species> ')
+	return mainn(input_name)
+	    
+if __name__ == '__main__':
+	main()
