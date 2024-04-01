80864c103a7d2cbb812169a295b228df0118a4f5
Leonid <120992466+ruscat003@users.noreply.github.com>
2024-04-01 23:03:52 +0300

    Update test_scraper.py

--- scrapers/test_scraper.py
+++ scrapers/test_scraper.py
@@ -1,31 +1,57 @@
 from LxmlSoup import LxmlSoup 
 import requests 
 import json 
+import time
  
-for i in range(6): 
- 
-    res = requests.get(F"https://plantpad.samlab.cn/api/disease/findById/{i}?fields=disease_info") 
-    data =res.json() 
- 
-    if(data["disease_info"]): 
-        title=data["disease"]["name"] 
-        definition_types=data["disease_info"]["definition_types"] 
-        structure_mode=data["disease_info"]["structure_mode"] 
-        period_environmental=data["disease_info"]["period_environmental"] 
-        overwintering=data["disease_info"]["overwintering"] 
-        chemical_control=data["disease_info"]["chemical_control"] 
-        physical_measures=data["disease_info"]["physical_measures"] 
-        biological_control=data["disease_info"]["biological_control"] 
-        symptoms=data["disease_info"]["symptoms"] 
-        signs=data["disease_info"]["signs"] 
-        resistance_level=data["disease_info"]["resistance_level"] 
-        apid_detection=data["disease_info"]["apid_detection"] 
-        infection_mechanism=data["disease_info"]["infection_mechanism"] 
-        stri= str(definition_types +"\n"+ structure_mode + "\n"+  period_environmental + "\n"+ overwintering + "\n"+ chemical_control  + "\n"+ physical_measures  + "\n"+  biological_control  + "\n" +symptoms + "\n" +  signs + "\n"+  resistance_level + "\n"+  apid_detection + "\n"+ infection_mechanism) 
-        while(stri.find("<a") != -1): 
-          start_index = stri.find("<a") 
-          end_index = stri.find("a>") 
-          stri = stri[:start_index] + stri[end_index + 2:] 
+for i in range(255400, 255800): 
+    timestart = time.time()
+    res = requests.post(f"https://plantpad.samlab.cn/api/disease/image/{i}") 
+    data = res.json() 
+  
+    if(data["species"]): 
+        title = data["species"]
+        discription = data["discription"]
+        symptoms = data["symptoms"]
+        signs = data["signs"]
+        type_of_disease = data["type_of_disease"]
+        mode_of_transmission = data["mode_of_transmission"]
+        development_process = data["development_process"]
+        environmental_conditions = data["environmental_conditions"]
+        overwintering = data["overwintering"]
+        chemical_control = data["chemical_control"]
+        physical_measures = data["physical_measures"]
+        biological_control = data["biological_control"]
+        agricultural_control = data["agricultural_control"]
+        resistance_level = data["resistance_level"]
+        apid_detection = data["apid_detection"]
+        infection_mechanism = data["infection_mechanism"]
+        genes_bacteria = data["genes_bacteria"]
+
+        words= str("name: " + title + "\n" + "discription: " + discription + "\n" 
+                  + "disease_symptoms: " + symptoms + "\n" + "signs_of_disease: " + signs + "\n"
+                  + "type_of_disease:" + chemical_control + "\n" + "mode_of_transmission: " + mode_of_transmission + "\n"
+                  + "development_process: " + development_process 
+                  + "environmental_conditions: " + "\n" + environmental_conditions + "\n" 
+                  + "overwintering_method: " + overwintering + "\n" + "prevention_strategies: " + "\n" 
+                  + "chemical_control: "+ chemical_control + "\n" 
+                  + "physical_measures: " + physical_measures + "\n" + "biological_control" + biological_control 
+                  + "agricultural_control: " + agricultural_control + "\n" + "pathogenic_mechanism:" + "\n" 
+                  + "resistance_level: " + resistance_level + "\n" + "apid_detection: " + apid_detection + "\n" 
+                  + "infection_mechanism: " + infection_mechanism + "\n" + "genes_bacteria: " + genes_bacteria) 
+        
+        while(words.find("<a") != -1): 
+          start_index = words.find("<a") 
+          end_index = words.find("a>") 
+          words = words[:start_index] + words[end_index + 2:] 
+        
+        words = words.replace('<p>', '')
+        words = words.replace('</p>', '')
+        words = words.replace('["', '')
+        words = words.replace('"]', '')
            
         with open(f"scraper/text_daset/{title}.txt", 'w', encoding="utf-8") as file: 
-            file.write(stri) 
+            file.write(words) 
+        timestop = time.time()
+        print(timestop - timestart)
+    else:
+        print("NO fail")
