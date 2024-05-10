db5ebda0b2f84e422e3b7fc94393bd5d81a7c1bc
Vasily Guryanov <62996833+RichelieuGVG1@users.noreply.github.com>
2024-05-10 23:26:38 +0300

    Add files via upload

--- yadisk downloader/ya1.py
+++ yadisk downloader/ya1.py
@@ -3,50 +3,61 @@ import shutil
 import time
 import zipfile
 import argparse
+import os
 
-def upload(path):
-	zipfilename=path.rsplit('/', 1)[1] + '.zip'
+global client
+
+def upload(path, client):
+	print(client)
+	if '/' in path:
+		zipfilename=path.rsplit('/', 1)[1] + '.zip'
+	else:
+		zipfilename=path+ '.zip'
 
 	with zipfile.ZipFile(zipfilename, 'w', zipfile.ZIP_DEFLATED) as zipf:
-        for root, dirs, files in os.walk(path):
-            for file in files:
-                zipf.write(os.path.join(root, file), os.path.relpath(os.path.join(root, file), path))
+		for root, dirs, files in os.walk(path):
+			for file in files:
+				zipf.write(os.path.join(root, file), os.path.relpath(os.path.join(root, file), path))
 
-    new_name = zipfilename.rsplit('.', 1)[1] + '.abc'
+	new_name = zipfilename.rsplit('.', 1)[0] + '.abc'
 	shutil.copyfile(zipfilename, new_name)
     # Загружает "file_to_upload.txt" в "/destination.txt"
 	client.upload("tomato1.abc", f"/Agro Filtered/{new_name}")
+	os.remove(zipfilename)
+	os.remove(new_name)
 	print(f'Шард загружен. Время выполнения: {round(time.time()-start)} сек.')
 
 
-def download(path):
-	client.download(f"/Agro Filtered/{path}", f"{path}.abc")
+def download(path, client):
+	client.download(f"/Agro Filtered/{path}.abc", f"{path}.abc")
 	shutil.copyfile(f"{path}.abc", f"{path}.zip")
 
 	with zipfile.ZipFile(f"{path}.zip", 'r') as zip_ref:
-    	zip_ref.extractall(f"{path}")
+		zip_ref.extractall(f"{path}")
 
+	os.remove(f"{path}.abc")
+	os.remove(f"{path}.zip")
 	print(f'Шард скачан и разархивирован. Время выполнения: {round(time.time()-start)} сек.')
 
 
 def main(file_path, operation):
 	with open('yandex token.txt', 'r') as file:
-    	token = file.read().strip()
+		token = file.read().strip()
 		client = yadisk.Client(token=token)
 
 	if client.check_token()==False:
 		print("Ошибка токена.")
-        exit()
+		exit()
 
-    if operation == "upload":
-        upload(file_path)
+	elif operation == "upload":
+		upload(file_path, client)
 
-    if operation == "download":
-        download(file_path)
+	elif operation == "download":
+		download(file_path, client)
 
-    else:
-        print("Неверно указана операция. Используйте -u для загрузки или -d для скачивания.")
-        exit()
+	else:
+		print("Неверно указана операция. Используйте -u для загрузки или -d для скачивания.")
+		exit()
 
 
 import time
